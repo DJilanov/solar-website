75e42a3cc96572836eddc0c889be701952edfe63
Dimitar Jilanov <dimitarjilanov@dimitars-macboo.local>
2022-05-01 15:30:43 +0300

    perc

--- site/index.js
+++ site/index.js
@@ -194,3 +194,19 @@ $(document).ready(function() {
   $(document).on("click", ".connect", handler);
 
 });
+
+function testWebP() {
+  return new Promise(res => {
+      const webP = new Image();
+      webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+      webP.onload = webP.onerror = () => {
+          res(webP.height === 2);
+      };        
+  })
+};
+
+testWebP().then(hasWebP => {
+  if(hasWebP) {
+    document.body.className = 'hasWEBP';
+  }
+});

74cdcf2ecbeda217fb7d92d42f1f8b8392c24865
Dimitar Jilanov <dimitarjilanov@dimitars-macboo.local>
2022-05-01 16:05:46 +0300

    rebuild

--- site/index.js
+++ site/index.js
@@ -195,22 +195,6 @@ $(document).ready(function() {
 
 });
 
-function testWebP() {
-  return new Promise(res => {
-      const webP = new Image();
-      webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
-      webP.onload = webP.onerror = () => {
-          res(webP.height === 2);
-      };        
-  })
-};
-
-testWebP().then(hasWebP => {
-  if(!hasWebP) {
-    document.body.className = 'noWEBP';
-  }
-});
-
 document.addEventListener("DOMContentLoaded", function() {
   var lazyloadImages;    
 
